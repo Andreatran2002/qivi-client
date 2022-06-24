92513011046e80d45ba292b3d07238aa21559999
andreatran2002 <andreatran2002@gmail.com>
2022-06-25 00:02:48 +0700

    init code

--- layouts/dashboard-layout.vue
+++ layouts/dashboard-layout.vue
@@ -3,10 +3,10 @@
     <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" app>
       <v-list-item class="px-2">
         <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          <img src="~/assets/images/logo.png"></img>
         </v-list-item-avatar>
 
-        <v-list-item-title>John Leider</v-list-item-title>
+        <v-list-item-title>Quỳnh Vy </v-list-item-title>
 
         <v-btn icon @click.stop="mini = !mini">
           <v-icon>mdi-chevron-left</v-icon>
